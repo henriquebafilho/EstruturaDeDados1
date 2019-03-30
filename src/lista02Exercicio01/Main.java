9fec34e1bba5b0caf54c20512fa0baaea8544d35
Henrique Filho <hbaf71@gmail.com>
2019-03-30 02:22:56 -0300

    Exercício 01 lista 2 feito

---
+++ src/lista02Exercicio01/Main.java
@@ -0,0 +1,31 @@
+package lista02Exercicio01;
+
+import filaCircularSimples.Fila;
+
+public class Main {	
+	public static void main(String[] args){
+		Fila f1 = new Fila(3);
+		f1.insere(1);
+		f1.insere(2);
+		f1.insere(3);
+		Fila f2 = new Fila(3);
+		f2.insere(4);
+		f2.insere(5);
+		f2.insere(6);
+		
+		combinaFilas(f1, f2);
+	}
+	
+	public static void combinaFilas(Fila f1, Fila f2) {
+		Fila filaMista = new Fila(f1.getTamanho()+f2.getTamanho());
+		
+		//Adicionando os elementos das filas à fila mista
+		for(int i = 0; i < filaMista.getTamanho(); i++) {
+			if(i%2==0) {
+				filaMista.insere(f1.remove());
+			} else {
+				filaMista.insere(f2.remove());
+			}
+		}
+	}
+}
