31b834cf2da07e23b7769762c58295c4e3c92c31
Henrique Filho <hbaf71@gmail.com>
2020-05-25 18:54:48 -0300

    identando

--- src/ArvoreBinaria/Main.java
+++ src/ArvoreBinaria/Main.java
@@ -4,9 +4,15 @@ import ArvoreBinariaBusca.ArvBinBusca;
 
 public class Main {
 	public static void main(String[] args) {
-		Arvbin<Integer> a1 = new Arvbin<Integer>(1), a2 = new Arvbin<Integer>(2), a3 = new Arvbin<Integer>(3, a1, a2),
-				a4 = new Arvbin<Integer>(4), a5 = new Arvbin<Integer>(5), a6 = new Arvbin<Integer>(6),
-				a7 = new Arvbin<Integer>(7, a5, a6), a8 = new Arvbin<Integer>(8), a9 = new Arvbin<Integer>(9),
+		Arvbin<Integer> a1 = new Arvbin<Integer>(1), 
+				a2 = new Arvbin<Integer>(2), 
+				a3 = new Arvbin<Integer>(3, a1, a2),
+				a4 = new Arvbin<Integer>(4), 
+				a5 = new Arvbin<Integer>(5), 
+				a6 = new Arvbin<Integer>(6),
+				a7 = new Arvbin<Integer>(7, a5, a6), 
+				a8 = new Arvbin<Integer>(8), 
+				a9 = new Arvbin<Integer>(9),
 				a10 = new Arvbin<Integer>(10, a8, a9);
 
 		a4.defineEsq(a3);
@@ -25,5 +31,7 @@ public class Main {
 		}
 		a4.mostra();
 		System.out.println();
+		
+		a3.mostra();
 	}
 }
